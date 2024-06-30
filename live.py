d056a5002734a3549f10057a964b10ec49cdeb51
Heightsdesign <plheric@gmail.com>
2024-06-30 18:12:43 +0200

    fine tuning dollar bars threshold 2

--- live.py
+++ live.py
@@ -32,7 +32,7 @@ stored_signal = None
 position_ids = {}
 
 
-def fetch_trades(minutes=60):
+def fetch_trades(minutes=240):
     conn = sqlite3.connect('trading_data.db')
     cursor = conn.cursor()
 
@@ -333,7 +333,7 @@ def manage_positions(symbol, size):
     calculate_average_move('XXBTZUSD')
 
     dollar_bars_trade_data = fetch_trades()
-    print(create_dollar_bars(dollar_bars_trade_data, 500000))
+    print(create_dollar_bars(dollar_bars_trade_data, 1000000))
 
     # Extract position details if there are open positions in the database
     if db_positions:
