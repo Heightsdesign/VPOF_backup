44a365b8ed9ada5d5c229d3cbd10f728f7bee453
Heightsdesign <plheric@gmail.com>
2024-07-05 19:35:38 +0200

    switiching to dollar bars 33

--- live.py
+++ live.py
@@ -271,13 +271,13 @@ def manage_positions(symbol, size, dollar_bars):
     if not open_positions['openPositions']:
         print('No open positions found.')
 
-        if stoch_setup == 'buy' and slope > 10:
+        if slope > 100:
             print('Placing new buy order.')
             place_order(order_auth, symbol, 'buy', size)
             take_profit, stop_loss = get_stops(dollar_bars, 'buy', current_price)
             insert_position(symbol, current_price, 'long', size, take_profit, stop_loss)
 
-        elif stoch_setup == 'sell' and slope < -10:
+        elif slope < -100:
             print('Placing new sell order.')
             place_order(order_auth, symbol, 'sell', size)
             take_profit, stop_loss = get_stops(dollar_bars, 'sell', current_price)
